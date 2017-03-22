4f9929bd0624ccc8f287ca46801cc5b8b2097ff8
Marc Herrera <marc.herrera@ntt.eu>
2017-03-22 13:44:08 +0100

    Amended for SG in non default VPCs

--- add_IPs_to_SG.sh
+++ add_IPs_to_SG.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-#
-#
-#
-#
+
 PATH="/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin"
 SCRIPT=`dirname $0`/`basename $0 .sh`
 
@@ -21,6 +18,7 @@ PROFILE=`cat $CONFIG | grep -v '\#' | grep PROFILE | sed 's/PROFILE=\(.*\)/\1/'`
 SG=`cat $CONFIG | grep -v '\#' | grep SG | sed 's/SG=\(.*\)/\1/'`
 URL=`cat $CONFIG | grep -v '\#' | grep URL | sed 's/URL=\(.*\)/\1/'`
 destdir=`cat $CONFIG | grep -v '\#' | grep stats | sed 's/stats=\(.*\)/\1/'`
+VPC=`cat $CONFIG | grep -v '\#' | grep VPC | sed 's/VPC=\(.*\)/\1/'`
 
 NS=$(dig +nocmd +multiline +noall +answer NS $(echo $URL | awk -F'.' {'print$2"."$3'}) | head -n1 | awk {'print$5'})
 query=`dig +nocmd +multiline +noall +answer $URL @$NS`
@@ -52,14 +50,14 @@ else
   IFS=$'\n'
   for ip in $oldIPs
     do
-      aws --profile $PROFILE ec2 revoke-security-group-ingress --group-name $SG --ip-permissions '[{"IpProtocol": "tcp", "FromPort": 80, "ToPort": 80,"IpRanges": [{"CidrIp": "'$ip'"}]}]'
-      aws --profile $PROFILE ec2 revoke-security-group-ingress --group-name $SG --ip-permissions '[{"IpProtocol": "tcp", "FromPort": 443, "ToPort": 443,"IpRanges": [{"CidrIp": "'$ip'"}]}]'
+      aws --profile $PROFILE ec2 revoke-security-group-ingress --group-id $SG --ip-permissions '[{"IpProtocol": "tcp", "FromPort": 80, "ToPort": 80,"IpRanges": [{"CidrIp": "'$ip'"}]}]'
+      aws --profile $PROFILE ec2 revoke-security-group-ingress --group-id $SG --ip-permissions '[{"IpProtocol": "tcp", "FromPort": 443, "ToPort": 443,"IpRanges": [{"CidrIp": "'$ip'"}]}]'
     done
   for ip in $query
     do
       ip=`echo $ip | awk '{print $5}'`
-      aws --profile $PROFILE ec2 authorize-security-group-ingress --group-name $SG --protocol tcp --port 80 --cidr $ip/32
-      aws --profile $PROFILE ec2 authorize-security-group-ingress --group-name $SG --protocol tcp --port 443 --cidr $ip/32
+      aws --profile $PROFILE ec2 authorize-security-group-ingress --group-id $SG --protocol tcp --port 80 --cidr $ip/32
+      aws --profile $PROFILE ec2 authorize-security-group-ingress --group-id $SG --protocol tcp --port 443 --cidr $ip/32
     done
   echo `echo $md5 > $destdir/$URL.lastmd5`
 fi
